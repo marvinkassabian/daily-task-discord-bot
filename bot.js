3a9a13896d806e579a595154176e706d14449682
Marvin Kassabian <marvin.kassabian@gmail.com>
2020-04-18 11:37:58 -0400

    fix: fix bad syntax in IIFE

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+(function main() {
+  const Discord = require('discord.js');
+  const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
+  client.on('ready', () => {
+    console.log('I am ready!');
+  });
 
-client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
-  }
-});
+  client.on('message', message => {
+    if (message.content === 'ping') {
+      message.reply('pong');
+    }
+  });
 
-client.login(process.env.BOT_TOKEN);
+  client.login(process.env.BOT_TOKEN);
 
-(main() {
 })();
